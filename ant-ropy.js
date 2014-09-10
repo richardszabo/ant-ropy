0e415a94a221162b1caad443f045002e48987351
Rics <richard_szabo@jataka.hu>
2014-09-10 16:54:08 +0200

    Separate js file with some dummy function.

--- ant-ropy.js
+++ ant-ropy.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    alert("started");
+var canvas;
+var ctx;
+var w;
+var h;
+
+function init(canvasid) {
+    //Canvas stuff
+    canvas = $(canvasid)[0];
+    ctx = canvas.getContext("2d");
+    w = $(canvasid).width();
+    h = $(canvasid).height();
+    init_hive();
+    init_food();
+}
+
+function init_hive() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(w/2-10, h/2-10,20, 20);
+}
+
+function init_food() {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(w/2-20, h/2-20,5, 5);
 }
