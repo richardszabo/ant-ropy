dc792d7b6dbdeac7f7ebf9c758510be1c9dd24f6
Rics <richard_szabo@jataka.hu>
2014-09-14 18:10:56 +0200

    Gaussian random. Several food around source.

--- ant-ropy.js
+++ ant-ropy.js
@@ -2,8 +2,9 @@ var canvas;
 var ctx;
 var width;
 var height;
-var food;
-var foodSize = 5;
+var foodNumber = 10;
+var food = [];
+var cellSize = 5;
 
 function init(canvasid) {
     //Canvas stuff
@@ -20,17 +21,41 @@ function init_hive() {
     ctx.fillRect(width/2-10, height/2-10,20, 20);
 }
 
+function gauss_random() {
+    // http://stackoverflow.com/a/20161247/21047 and http://jsfiddle.net/Guffa/tvt5K/
+    return ((Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random()-3)) / 3;
+} 
+
+function get2DGaussian(mean, deviation) {
+    var point = new Point();
+    point.x = (gauss_random() + 1) / 2 * deviation + mean.x;
+    point.y = (gauss_random() + 1) / 2 * deviation + mean.y;
+    return point;
+}
+
 function init_food() {
-    food = new Food();
-    food.setX(Math.floor(Math.random() * width));
-    food.setY(Math.floor(Math.random() * height));
     ctx.fillStyle = "yellow";
-    ctx.fillRect(food.getX(), food.getY(), 5, 5);
+    var foodSource = new Point();
+    foodSource.x = Math.floor((gauss_random() + 1 ) / 2 * width);
+    foodSource.y = Math.floor((gauss_random() + 1 ) / 2 * height);
+    for(i = 0; i < foodNumber; ++i ) {
+	var point = get2DGaussian(foodSource,50);
+	//alert('i:' + i + ' ' + point.x + ' ' + point.y + ':');
+	food[i] = new Food();
+	food[i].setX(Math.floor(point.x));
+	food[i].setY(Math.floor(point.y));
+	ctx.fillRect(food[i].getX(), food[i].getY(), cellSize, cellSize);
+    } 
+}
+
+function Point () {
+    var x;
+    var y;
 }
 
 function Food () {
-    this.x = 0;
-    this.y = 0;
+    var x = 0;
+    var y = 0;
 }
  
 Food.prototype.getX = function() {
