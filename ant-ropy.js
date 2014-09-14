4ff8a18a08d40438dc3c2b0a6e189c08f6a2c0e4
Rics <richard_szabo@jataka.hu>
2014-09-14 12:15:05 +0200

    Starting Food class. Random.

--- ant-ropy.js
+++ ant-ropy.js
@@ -1,24 +1,51 @@
 var canvas;
 var ctx;
-var w;
-var h;
+var width;
+var height;
+var food;
+var foodSize = 5;
 
 function init(canvasid) {
     //Canvas stuff
     canvas = $(canvasid)[0];
     ctx = canvas.getContext("2d");
-    w = $(canvasid).width();
-    h = $(canvasid).height();
+    width = $(canvasid).width();
+    height = $(canvasid).height();
     init_hive();
     init_food();
 }
 
 function init_hive() {
     ctx.fillStyle = "red";
-    ctx.fillRect(w/2-10, h/2-10,20, 20);
+    ctx.fillRect(width/2-10, height/2-10,20, 20);
 }
 
 function init_food() {
+    food = new Food();
+    food.setX(Math.floor(Math.random() * width));
+    food.setY(Math.floor(Math.random() * height));
     ctx.fillStyle = "yellow";
-    ctx.fillRect(w/2-20, h/2-20,5, 5);
+    ctx.fillRect(food.getX(), food.getY(), 5, 5);
 }
+
+function Food () {
+    this.x = 0;
+    this.y = 0;
+}
+ 
+Food.prototype.getX = function() {
+    return this.x;
+};
+
+Food.prototype.getY = function() {
+    return this.y;
+};
+
+Food.prototype.setX = function(x) {
+    return this.x = x;
+};
+
+Food.prototype.setY = function(y) {
+    return this.y = y;
+};
+
