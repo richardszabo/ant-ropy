1a86605f2fe4323130821b12c61dc5cc56b8c442
Rics <richard_szabo@jataka.hu>
2014-11-13 09:31:09 +0100

    Particle object is parent of Hive as well.

--- hive.js
+++ hive.js
@@ -1,20 +1,23 @@
 "use strict";
 
+Hive.prototype = Object.create(Particle.prototype);
+Hive.prototype.constructor = Hive;
+
 function Hive () {
     this.hiveDrawSize = 10;
+    this.pos2D = AntSpace.center();
 }
 
 Hive.prototype.draw = function(ctx) {
     ctx.fillStyle = "blue";
     ctx.beginPath();
-    var canvaspoint = AntSpace.point2Canvas(AntSpace.center());
+    var canvaspoint = AntSpace.point2Canvas(this.pos2D);
     ctx.arc(canvaspoint.x,canvaspoint.y,this.hiveDrawSize,0,2*Math.PI);
     ctx.fill();
 }
 
 Hive.prototype.isIn = function(pos) {
-    var hive = AntSpace.center();
-    return (pos.x - hive.x) * (pos.x - hive.x) +
-	(pos.y - hive.y) * (pos.y - hive.y) <
+    return (pos.x - this.x) * (pos.x - this.x) +
+	(pos.y - this.y) * (pos.y - this.y) <
 	this.hiveDrawSize / AntSpace.cellSize * this.hiveDrawSize / AntSpace.cellSize;
 }
