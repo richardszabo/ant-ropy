c91c35511fbf0b5aacbef053b54bce9338448a8b
Rics <richard_szabo@jataka.hu>
2014-11-13 11:46:51 +0100

    Pheromone refactored to avoid duplication of pheromone calculation.

--- pheromone.js
+++ pheromone.js
@@ -1,27 +1,37 @@
 "use strict";
 
+function PheromoneMatrix() {
+    this.read_matrix = create2DArray(AntSpace.spaceSize);
+    this.write_matrix = create2DArray(AntSpace.spaceSize);    
+}
+
+PheromoneMatrix.prototype.update = function() {
+    this.read_matrix = [];
+    copy2DArray(this.write_matrix, this.read_matrix);
+}
+
 function Pheromone(antropy) {
     this.antropy = antropy;
-    this.hive_read_matrix = create2DArray(AntSpace.spaceSize);
-    this.hive_write_matrix = create2DArray(AntSpace.spaceSize);    
-    this.food_read_matrix = create2DArray(AntSpace.spaceSize);
-    this.food_write_matrix = create2DArray(AntSpace.spaceSize);    
+    this.hive_matrix = new PheromoneMatrix();
+    this.food_matrix = new PheromoneMatrix();
     this.evap_rate = 0.01;
     this.diffusion_constant = 0.86;
 }
 
 Pheromone.prototype.step = function() {
-    this.diffuse();
-    this.calculate();
+    this.diffuse(this.hive_matrix);
+    this.diffuse(this.food_matrix);
+    this.calculate(this.hive_matrix,true);
+    this.calculate(this.food_matrix,false);
     this.update();
 }
 
 Pheromone.prototype.draw = function(ctx) {
     // http://stackoverflow.com/a/13916313/21047
-    for(var i = 0; i < this.hive_read_matrix.length; ++i ) {
-	for(var j = 0; j < this.hive_read_matrix[i].length; ++j ) {
-	    var hivefillValue = Math.min(Math.round(this.hive_read_matrix[i][j] || 0),255); // for overflow
-	    var foodfillValue = Math.min(Math.round(this.food_read_matrix[i][j] || 0),255); // for overflow
+    for(var i = 0; i < this.hive_matrix.read_matrix.length; ++i ) {
+	for(var j = 0; j < this.hive_matrix.read_matrix[i].length; ++j ) {
+	    var hivefillValue = Math.min(Math.round(this.hive_matrix.read_matrix[i][j] || 0),255); // for overflow
+	    var foodfillValue = Math.min(Math.round(this.food_matrix.read_matrix[i][j] || 0),255); // for overflow
 		    ctx.fillStyle = "rgb(0," + foodfillValue + "," + hivefillValue + ")"; 
 		var canvaspoint = AntSpace.point2Canvas(new Point(i,j));
 		ctx.fillRect(canvaspoint.x-AntSpace.cellSize/2,canvaspoint.y-AntSpace.cellSize/2,AntSpace.cellSize,AntSpace.cellSize);
@@ -29,68 +39,38 @@ Pheromone.prototype.draw = function(ctx) {
     }
 }
 
-Pheromone.prototype.calculate = function() {
-    // hive
+Pheromone.prototype.calculate = function(matrix,isHive) {
     for( var i = 0; i < Ants.antNumber; ++i ) {
-	this.hive_write_matrix[this.antropy.ants.ant[i].x][this.antropy.ants.ant[i].y] = 
-	    (this.hive_write_matrix[this.antropy.ants.ant[i].x][this.antropy.ants.ant[i].y] || 0) + this.antropy.ants.ant[i].getEmittedPheromoneHive();
-    }
-    // food
-    for( var i = 0; i < Ants.antNumber; ++i ) {
-	this.food_write_matrix[this.antropy.ants.ant[i].x][this.antropy.ants.ant[i].y] = 
-	    (this.food_write_matrix[this.antropy.ants.ant[i].x][this.antropy.ants.ant[i].y] || 0) + this.antropy.ants.ant[i].getEmittedPheromoneFood();
+	matrix.write_matrix[this.antropy.ants.ant[i].x][this.antropy.ants.ant[i].y] = 
+	    (matrix.write_matrix[this.antropy.ants.ant[i].x][this.antropy.ants.ant[i].y] || 0) + 
+	    (isHive ? this.antropy.ants.ant[i].getEmittedPheromoneHive() : this.antropy.ants.ant[i].getEmittedPheromoneFood());
     }
 }
 
-Pheromone.prototype.diffuse = function() {
-    // hive
+Pheromone.prototype.diffuse = function(matrix) {
     // modifying write matrix according to the modified Diffuse2D source code in ant-ropy project
-    for(var i = 0; i < this.hive_write_matrix.length; ++i ) {
-	for(var j = 0; j < this.hive_write_matrix[i].length; ++j ) {
+    for(var i = 0; i < matrix.write_matrix.length; ++i ) {
+	for(var j = 0; j < matrix.write_matrix[i].length; ++j ) {
 	    var avg = 0;
 	    for( var k = 0; k < 8; ++k ) {
-		avg += this.hive_read_matrix[(i + Ants.NEIGHBOURS[k][0] + AntSpace.spaceSize)%AntSpace.spaceSize]
-		                       [(j + Ants.NEIGHBOURS[k][1] + AntSpace.spaceSize)%AntSpace.spaceSize] || 0;
+		avg += matrix.read_matrix[AntSpace.crop2Space(i + Ants.NEIGHBOURS[k][0])]
+		                            [AntSpace.crop2Space(j + Ants.NEIGHBOURS[k][1])] || 0;
 	    }
 	    /* value diffusing from neighbours */
             var d = avg/8 * (1 - this.diffusion_constant);
 	    /* cell value losing some parts by diffusion */
-	    var val = this.diffusion_constant * (this.hive_read_matrix[i][j] || 0);
+	    var val = this.diffusion_constant * (matrix.read_matrix[i][j] || 0);
 	    /* cell value getting the value of the neigbours */
 	    val += d;
 	    /* cell value after evaporation */
 	    val *= (1 - this.evap_rate);
-	    this.hive_write_matrix[i][j] = val;
-	}
-    }
-    // food
-    // modifying write matrix according to the modified Diffuse2D source code in ant-ropy project
-    for(var i = 0; i < this.food_write_matrix.length; ++i ) {
-	for(var j = 0; j < this.food_write_matrix[i].length; ++j ) {
-	    var avg = 0;
-	    for( var k = 0; k < 8; ++k ) {
-		avg += this.food_read_matrix[(i + Ants.NEIGHBOURS[k][0] + AntSpace.spaceSize)%AntSpace.spaceSize]
-		                       [(j + Ants.NEIGHBOURS[k][1] + AntSpace.spaceSize)%AntSpace.spaceSize] || 0;
-	    }
-	    /* value diffusing from neighbours */
-            var d = avg/8 * (1 - this.diffusion_constant);
-	    /* cell value losing some parts by diffusion */
-	    var val = this.diffusion_constant * (this.food_read_matrix[i][j] || 0);
-	    /* cell value getting the value of the neigbours */
-	    val += d;
-	    /* cell value after evaporation */
-	    val *= (1 - this.evap_rate);
-	    this.food_write_matrix[i][j] = val;
+	    matrix.write_matrix[i][j] = val;
 	}
     }
 }
 
 Pheromone.prototype.update = function() {
-    // hive
-    this.hive_read_matrix = [];
-    copy2DArray(this.hive_write_matrix, this.hive_read_matrix);
-    // food
-    this.food_read_matrix = [];
-    copy2DArray(this.food_write_matrix, this.food_read_matrix);
+    this.hive_matrix.update();
+    this.food_matrix.update();
 }
 
