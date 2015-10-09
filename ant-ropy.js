2527d9f98c015e6212bc9f24d49b04ffe650ddbd
Rics <richard_szabo@jataka.hu>
2015-10-09 11:28:04 +0200

    Fixing ALL COLLECTED appearance. Stopping run when finished.

--- ant-ropy.js
+++ ant-ropy.js
@@ -6,6 +6,7 @@ function Antropy () {
     this.foodSource;
     this.foodNumber;
     this.stepNumber;
+    this.run = null;
 }
 
 Antropy.prototype.setCanvases = function(p_canvasid, p_pheromone_canvasid) {
@@ -18,6 +19,14 @@ Antropy.prototype.setEntropyCanvases = function(p_entropy_canvasid, p_plot_canva
     this.plot_canvasid = p_plot_canvasid;
 }
 
+Antropy.prototype.setWindow = function(p_window) {
+    this.window = p_window;
+}
+
+Antropy.prototype.setButton = function(p_button) {
+    this.button = p_button;
+}
+
 Antropy.prototype.reset = function() {
     this.seed = document.getElementById("seed").value;
     this.antNumber = document.getElementById("antnum").value;
@@ -25,6 +34,7 @@ Antropy.prototype.reset = function() {
     this.foodNumber = document.getElementById("foodnum").value;
     Math.seedrandom(this.seed);
     this.inited = false;
+    this.run = null;
     this.init();
 }
 
@@ -62,11 +72,15 @@ Antropy.prototype.step = function() {
     this.pheromone.step();
     this.ants.step();
 
+    if( this.hive.getFood() == this.foods.maxFood && this.run ) {
+        this.toggle();
+    }
+
     this.draw();
     var end =  +new Date();  // log end timestamp
     var diff = end - start;
     document.getElementById("speed").value = diff;
-    document.getElementById("food").value = this.hive.getFood() + "/" + this.foods.maxFood + (this.hive.getFood() === this.foods.maxFood ? " ALL COLLECTED" : "");
+    document.getElementById("food").value = this.hive.getFood() + "/" + this.foods.maxFood + (this.hive.getFood() == this.foods.maxFood ? " ALL COLLECTED" : "");
     document.getElementById("stepnum").value = ++this.stepNumber;
     document.getElementById("ant_food").value = this.ants.carryingFood;
     document.getElementById("ant_search").value = this.ants.antNumber - this.ants.carryingFood;
@@ -83,6 +97,16 @@ Antropy.prototype.step = function() {
         document.getElementById("ant_mode").value = selected_ant.mode === Ants.MODE_SEARCH ? "SEARCH" : "HOME";
         var foodStr = "";
         var hiveStr = "";
+    }
+}
+
+Antropy.prototype.toggle = function() {
+    if (this.run){
+        this.run = this.window.clearInterval(this.run);
+        this.button.innerHTML = 'Run';
+    } else {
+        this.run = this.window.setInterval(function(){ antropy.step(); },10);
+        this.button.innerHTML = 'Stop';
     }
 }
 
